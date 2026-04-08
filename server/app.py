a750eeb2b8ed21d4ce4a508627b443f7752ab7b9
Venisha Dsouza <venisha574347@gmail.com>
2026-04-08 22:11:26 +0530

    added server/app.py

--- server/app.py
+++ server/app.py
@@ -2,4 +2,11 @@ import sys
 import os
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from app import app, main
+from app import app
+
+def main():
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+if __name__ == "__main__":
+    main()
